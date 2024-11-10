7d36a4976c7176c2577d7d7b9be11cbdd18d336c
JadirJunior <jadirjunior8@gmail.com>
2024-11-10 20:25:22 +0000

    Adding UserService and UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,51 @@
+import { BaseController } from "../base/BaseController";
+import UserService from "../services/UserService";
+import User from "../database/models/User";
+import { NextFunction, Request, Response } from "express";
+import { createToken } from "../auth/auth";
+
+class UserServiceController extends BaseController<User> {
+	protected readonly service: UserService;
+
+	constructor(service: UserService) {
+		super(service);
+		this.service = service;
+	}
+
+	async login(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { username, password } = req.body;
+			const { message, status, data } = await this.service.login(
+				username,
+				password
+			);
+
+			if (!data) {
+				res.status(status ?? 200).json({ message });
+				return;
+			}
+
+			const token = createToken(data);
+
+			res.status(status ?? 200).json({ message, data: token });
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	async createUser(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { username, password } = req.body;
+			const { message, status, data } = await this.service.createUser(
+				username,
+				password
+			);
+
+			res.status(status ?? 200).json({ message, data });
+		} catch (error) {
+			next(error);
+		}
+	}
+}
+
+export default UserServiceController;
---
+++ src/services/UserService.ts
@@ -0,0 +1,80 @@
+import { ModelStatic } from "sequelize";
+import { BaseService } from "../base/BaseService";
+import User from "../database/models/User";
+import { CommSensoResponse } from "../utils/CommSensoResponse";
+import { compareHash, generateHash } from "../utils/cripto";
+
+class UserService extends BaseService<User> {
+	protected model: ModelStatic<User> = User;
+
+	constructor() {
+		super(User);
+	}
+
+	public async login(
+		username: string,
+		password: string
+	): Promise<CommSensoResponse<User>> {
+		const user = await this.model.findOne({
+			where: {
+				username,
+			},
+		});
+
+		if (!user) {
+			return {
+				status: 404,
+				message: "User not found",
+				data: undefined,
+			};
+		}
+
+		if (compareHash(password, user.password)) {
+			return {
+				status: 200,
+				message: "User found",
+				data: user,
+			};
+		}
+
+		return {
+			status: 401,
+			message: "Unauthorized",
+			data: undefined,
+		};
+	}
+
+	public async createUser(
+		username: string,
+		password: string
+	): Promise<CommSensoResponse<User>> {
+		const user = await this.model.findOne({
+			where: {
+				username,
+			},
+		});
+
+		if (user) {
+			return {
+				status: 409,
+				message: "User already exists",
+				data: undefined,
+			};
+		}
+
+		const hash = generateHash(password);
+
+		const newUser = await this.model.create({
+			username,
+			password: hash,
+		});
+
+		return {
+			status: 201,
+			message: "User created",
+			data: newUser,
+		};
+	}
+}
+
+export default UserService;
